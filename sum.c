b56b8522885e6edf787fdc3bb3867302f80874e6
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-16 23:28:02 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter first num:");
+	scanf("%d",&a);
+	printf("Enter second num:");
+	scanf("%d",&b);
+	printf("Sum id %d",a+b);
+	return 0;
+}
+
