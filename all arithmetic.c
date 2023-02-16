b136946b17544c1be6361ccc5bcf4adca6fa99cc
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-16 23:28:23 +0530

    Add files via upload

---
+++ all arithmetic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter first num:");
+	scanf("%d",&a);
+	printf("Enter second num:");
+	scanf("%d",&b);
+	printf("Sum is %d\n",a+b);
+	printf("diff is %d\n",a-b);
+	printf("product is %d\n",a*b);
+	printf("division is %d\n",a/b);
+	printf("Modulus is %d",a%b);
+	return 0;
+}
+
