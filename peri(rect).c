a38b65164f6fe021a10d8e90a28125cddffd7b38
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-16 23:40:56 +0530

    Add files via upload

---
+++ peri(rect).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int l,b;
+	printf("Enter length:");
+	scanf("%d",&l);
+	printf("Enter breathe:");
+	scanf("%d",&b);
+	printf("perimeter is %d",2*(l+b));
+	return 0;
+}
+
