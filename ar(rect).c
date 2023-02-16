841b53b81ab04e49c29c4c7b35afe4caaa3cd056
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-16 23:41:15 +0530

    Add files via upload

---
+++ ar(rect).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int l,b;
+	printf("Enter length:");
+	scanf("%d",&l);
+	printf("Enter breathe:");
+	scanf("%d",&b);
+	printf("Area is %d",(l*b));
+	return 0;
+}
+
