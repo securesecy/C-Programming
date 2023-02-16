7f25568837dcee951bb2047b9f7a1f60cec7bdb6
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-16 23:19:56 +0530

    Add files via upload

---
+++ basics.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+	int a;
+	float b;
+	char c;
+	double d;
+	printf("Enter Character Value : ");
+	scanf("%c", &c);
+	printf("Enter Integer Value : ");
+	scanf("%d", &a);
+	printf("Enter Float Value : ");
+	scanf("%f", &b);
+	printf("Enter Double Value : ");
+	scanf("%lf", &d);
+	printf("Integer %d \nFloat %f \nCharacter %c \nDouble %lf", a, b, c, d);
