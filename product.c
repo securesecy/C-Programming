9032a30ea962d3164a3e4c66b7deca249eb8bc49
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-17 23:12:28 +0530

    Add files via upload

---
+++ product.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main()
+{
+    int n1,n2,a;
+    printf("Enter first number");
+    scanf("%d",&n1);
+    printf("Enter second number");
+    scanf("%d",&n2);
+    a=n1*n2;
+    printf("Product is %d",a);
+    return 0;    
+    
+    
+}
+  
+    
+
