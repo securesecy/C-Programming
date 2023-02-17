dd71287218eb03795db1076ee3390579c4cbfcd2
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-17 23:09:21 +0530

    Add files via upload

---
+++ numbersystem.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int value;
+    scanf("%d",&value);
+    printf("%i\n",value);
+    printf("%o\n",value);
+    printf("%x\n",value);
+    printf("%X",value);
+   
+    return 0;
+}
+
