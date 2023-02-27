5ffc63b81282dc9d4ebc904ab90e02aacc929cf2
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-02-27 22:44:49 +0530

    Create hellome.c

---
+++ hellome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char name[1000000];
+    scanf("%s", name);
+    printf("Welcome %s", name);    
+    return 0;
+}
